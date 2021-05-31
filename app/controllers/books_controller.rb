d8348ed7dfead4fd078e971d59b3b217c77465b8
Morris Buel <morris.buel@gmail.com>
2021-05-31 10:59:15 -0700

    completed up to 4.6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def create
     new_book = Book.create(name: params[:name])
-    render json: { book: new_book }
+
+    render '../../app/views/books/create.jbuilder'
   end
 end
