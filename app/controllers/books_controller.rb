1bd85837d2b638ac792f03d4edc9578631a9fa1a
Morris Buel <morris.buel@gmail.com>
2021-05-31 11:09:03 -0700

    books example completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def create
-    new_book = Book.create(name: params[:name])
+    @book = Book.create(name: params[:name])
 
-    render '../../app/views/books/create.jbuilder'
+    render 'books/create'
   end
 end
